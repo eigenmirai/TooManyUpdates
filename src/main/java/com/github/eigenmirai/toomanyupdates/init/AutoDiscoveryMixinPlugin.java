ca265b773913ef449f6b52c3602b5baa6cfb70c2
GitHub Action <action@github.com>
2024-02-19 14:46:14 +0000

    Initialize template repository proper
    Set package name to com.github.eigenmirai.toomanyupdates
    Set project name to TooManyUpdates
    Set modid to toomanyupdates

--- src/main/java/com/github/eigenmirai/toomanyupdates/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/eigenmirai/toomanyupdates/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.eigenmirai.toomanyupdates.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
